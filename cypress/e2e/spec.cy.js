71aa484bf9e23c245fa66ab737a1570c91397f96
Jason Lui <lui.jason10@gmail.com>
2025-09-28 13:06:08 -0400

    Created a few tests for row 2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -42,4 +42,35 @@ describe("template spec", () => {
     cy.contains("Load Delay").click();
     cy.contains(/Button Appearing After Delay/i).click();
   });
+
+  it("Row 2, Long Waits", () => {
+    //AJAX Data
+    cy.visit("http://uitestingplayground.com/");
+    cy.contains("AJAX Data").click();
+    cy.intercept("GET", "/ajaxdata").as("ajaxRequest");
+    cy.contains(/Button Triggering AJAX Request/i).click();
+    cy.wait("@ajaxRequest");
+    cy.contains("Data loaded with AJAX get request.");
+
+    //Client Side Delay
+    cy.visit("http://uitestingplayground.com/");
+    cy.contains("Client Side Delay").click();
+    cy.contains(/Button Triggering Client Side Logic/i).click();
+
+    /*
+    Because of how Cypress deals with waiting and timeouts there isn't a way I currently see
+    to dynamically wait for a client delay 
+    */
+    cy.wait(15000);
+
+    cy.contains("Data calculated on the client side.");
+  });
+
+  it.only("Rest of Row 2", () => {
+    //Click
+    cy.visit("http://uitestingplayground.com/");
+    cy.contains("Click").click();
+    cy.contains(/Button That Ignores DOM Click Event/i).click();
+    cy.contains(/Button That Ignores DOM Click Event/i).click();
+  });
 });
